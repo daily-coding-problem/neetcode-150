3fcb3d011262f67501d879febc4e9766cc4bd661
Nicholas Adamou <nicholasadamouemail@gmail.com>
2025-01-12 16:33:59 -0500

    Add solution for 'Best Time to Buy and Sell Stock'
    Implemented a function to calculate maximum profit from stock prices using a greedy approach. Included unittests to verify correctness of the solution.

---
+++ greedy/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,19 @@
+# https://neetcode.io/problems/buy-and-sell-crypto
+
+import unittest
+from typing import List
+
+def max_profit(prices: List[int]) -> int:
+    result = 0
+    min_price = float('inf')
+
+    for price in prices:
+        min_price = min(min_price, price)
+        result = max(result, price - min_price)
+
+    return result
+
+class Test(unittest.TestCase):
+    def test_max_profit(self):
+        self.assertEqual(max_profit([10, 1, 5, 6, 7, 1]), 6)
+        self.assertEqual(max_profit([10, 8, 7, 5, 2]), 0)
