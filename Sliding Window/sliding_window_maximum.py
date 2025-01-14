e19d04a71fab4afffcc4a93b880740a10e50b13f
Nicholas Adamou <nicholasadamouemail@gmail.com>
2025-01-14 18:06:53 -0500

    Add sliding window maximum solution with tests
    Implemented the `max_sliding_window` function to solve the sliding window maximum problem using a heap. Included a unit test to verify the implementation against a test case for correctness.

---
+++ Sliding Window/sliding_window_maximum.py
@@ -0,0 +1,24 @@
+# https://neetcode.io/problems/sliding-window-maximum
+
+import unittest
+from typing import List
+import heapq
+
+def max_sliding_window(nums: List[int], k: int) -> List[int]:
+    result = []
+    heap = []
+
+    for i, num in enumerate(nums):
+        heapq.heappush(heap, (-num, i)) # Add number to the current window maximum
+
+        if i >= k - 1: # Did we fall outside the window?
+            while heap[0][1] <= i - k: # Shrink the window
+                heapq.heappop(heap)
+
+            result.append(-heap[0][0]) # Add the current maximum in the window to the result
+
+    return result
+
+class Test(unittest.TestCase):
+    def test_max_sliding_window(self):
+        self.assertEqual(max_sliding_window([1, 2, 1, 0, 4, 2, 6], 3), [2, 2, 4, 4, 6])
